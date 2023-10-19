7e9486bb9d7ee68d8d814fe9f3ced3bd0717079e
silviahmungei <silviahmungei873@gmail.com>
2023-10-19 01:36:40 -0700

    Upload

---
+++ button.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Button1.Text = "Click
+ Me" ' Set the name of button 
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("
+ Visit
+ here:
+ https://www.javatpoint.com")     ' Display the message, when a user clicks on Click me button 
+    End Sub
+End Class
